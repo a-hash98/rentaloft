e7665cee79fbd2c07f48c8ac5ed17d8db32a5014
A Hashim <ahashim983@yahoo.com>
2020-02-19 20:21:31 +0000

    loft type filter debug

--- src/components/LoftsContainer.js
+++ src/components/LoftsContainer.js
@@ -11,11 +11,13 @@ const LoftsContainer = () => {
         {
             (value) => {
                 const {loading, sortedLofts, lofts} = value
+                // console.log(lofts)
+                // console.log('inside lofts container')
                 return (
                     <div>
                         {/* <LoftsFilter lofts={lofts} /> */}
+                        <LoftsFilter lofts={lofts} />
                         <LoftsList lofts={sortedLofts} />
-                        <LoftsFilter />
                     </div>
                 )
             }
--- src/components/LoftsFilter.js
+++ src/components/LoftsFilter.js
@@ -1,28 +1,37 @@
 import React from 'react'
 import { useContext } from 'react'
-import { LostContext } from '../Context'
+import { LoftContext } from '../Context'
 import Title from '../components/Title'
 
-const uniqueOptions = (items, type) => {
-    return [...new Set(items.map(item => item[type]))]
+const getUniqueOptions = (items, value) => {
+    return [...new Set(items.map(item => item[value]))]
 }
-const LoftsFilter = () => {
-    const context = useContext(RoomContext);
+const LoftsFilter = ({lofts}) => {
+
+    const context = useContext(LoftContext);
     const {
-        handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets } = comtext
-    }
+        handleChange, type, capacity, price, minPrice, maxPrice, minSize, maxSize, breakfast, pets } = context
+
+    
+    let options = getUniqueOptions(lofts, "type")
+    options = ["all", ...options]
+    options = options.map((item, index) => {
+          return <option value={item} key={index}>{item}</option>
+    })
+
     return (
         <section className="filter-container">
            <Title title='search lofts' />
-           <form className="filter-form">
+        <form className="filter-form">
                <div className="form-group">
                    <label htmlFor="type">loft type</label>
                    <select name="type" id="" value={type} className="form-control" onChange={handleChange}>
-
+                    {options}
                    </select>
-           </form>
+                </div>
+           </form> 
         </section>    
     )
-}
 
+}
 export default LoftsFilter
